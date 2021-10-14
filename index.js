f03cc7b23cec9cd1f48c92cb58b55609c00196a7
Eagledb14 <eagledb14@gmail.com>
2021-10-14 01:36:42 -0600

    added bored section and api, made the text smaller

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function info() {
+// function info() {
+window.addEventListener('load', event => {
     let infoUrl = "https://api.countapi.xyz/info/db14.net/test";
 
     fetch(infoUrl)
@@ -9,12 +10,37 @@ function info() {
             console.log(json);
             document.getElementById('the-button').innerHTML = json.value;
         })
-}
+
+    let activityUrl = "http://www.boredapi.com/api/activity/";
+
+    for (let i = 0; i < 10; i++) {
+        fetch(activityUrl)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            console.log(json);
+            let newList = document.createElement('li');
+            newList.innerHTML = json.activity;
+            document.getElementById("activities").appendChild(newList);
+        })
+    }
+})
+
+window.addEventListener('resize', (event) => {
+    let dimension = window.innerWidth * 0.25;
+
+    document.getElementById('the-button').style.width = dimension.toString() + "px";
+    document.getElementById('the-button').style.height = dimension.toString() + "px";
+})
 
 document.getElementById('the-button').addEventListener("click", event => {
     event.preventDefault();
     document.getElementById('the-button').disabled = true;
 
+    if (document.getElementById('the-button').innerHTML == "0") {
+        return;
+    }
 
     let subtractUrl = "https://api.countapi.xyz/update/db14.net/test?amount=-1";
 
@@ -28,7 +54,7 @@ document.getElementById('the-button').addEventListener("click", event => {
 
             setTimeout(() => {
                 document.getElementById('the-button').disabled = false;
-            }, 10000);
+            }, 5000);
         });
 });
 
